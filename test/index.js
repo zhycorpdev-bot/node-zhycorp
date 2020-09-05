d53d2c3a83ecc1a65328718a3c39f5c87a9c4df1
KurokuTetsuya <perumnas.good@gmail.com>
2020-09-05 10:59:56 +0700

    fix(test): Fix test file

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-const { ZhycorpWrapper } = require("../dist/Zhycorp");
+const { ZhycorpWrapper } = require("../dist/");
 (async function() {
     console.log(await new ZhycorpWrapper().getBot("364242246314360843"))
 })();
