47f78cee17218757b63fe14b277e58101bc4133e
mozartrafi <mozartrafikt@gmail.com>
2020-09-25 11:03:23 +0700

    docs: Update djs example

--- examples/discord.js/index.js
+++ examples/discord.js/index.js
@@ -9,20 +9,21 @@ client.on("ready", () => {
 
 client.on("message", async (message) => {
     if (!message.guild || message.author.bot) return;
-    const prefix = "$";
+    const prefix = "!";
     const args = message.content.substring(prefix.length).split(/ +/g);
     const command = args.shift();
-    
+
     if (command === "zhycorp") {
-        const user = message.mentions.users.first() || await  client.fetchUser(args[0]);
+        const user = message.mentions.users.first() || await client.fetchUser(args[0]);
         if (!user || !user.bot) return message.channel.send("Please mention a bot");
         const result = await Zhycorp.getBot(user.id);
-        if (!result) return message.channel.send("That bot is not registered to zhycorp database");
+        if (!result) return message.channel.send("That bot is not registered on Zhycorp database");
         const embed = new MessageEmbed()
-            .setAuthor("Zhycorp Bot")
+            .setColor("RANDOM")
+            .setAuthor("Zhycorp Bot Informations")
             .addField("Bot Information", `\`\`\`\nBot Name: ${user.tag}\nBot ID: ${user.id}\nPrefix: ${result.prefix}\n\`\`\``)
             .addField("Owner Information", `\`\`\`\nOwner Name: ${result.owner.userTag}\nOwner ID: ${result.owner.userID}\n\`\`\``)
-            .setColor("RANDOM");
+            .setFooter("Powered by node-zhycorp", "https://api.zhycorp.xyz/assets/images/logo.png");
         return message.channel.send(embed);
     }
 });
