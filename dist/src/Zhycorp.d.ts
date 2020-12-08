002ae2c67a7bdb9caf14b45d7c51b5c0c7a64727
mozartrafi <mozartrafi@users.noreply.github.com>
2020-12-08 23:42:04 +0000

    chore(build) Build from 950f1eaf34a679ba5693b5fa5188cfcff90effe3

--- dist/src/Zhycorp.d.ts
+++ dist/src/Zhycorp.d.ts
@@ -1,4 +1,4 @@
-import type { Bot } from "./interfaces";
+import { Bot } from "./interfaces";
 export declare class ZhycorpWrapper {
     private readonly baseURL;
     getBot(id: string): Promise<Bot>;
